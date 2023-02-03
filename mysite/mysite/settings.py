8b7ad080af606cc78cc5c4d38ee9b4dfca3adb65
asifdotpy <asifdotpy@gmail.com>
2023-02-04 00:04:09 +0600

    added jobs page for the /jobs url

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -33,6 +33,7 @@ ALLOWED_HOSTS = []
 
 INSTALLED_APPS = [
     'landing.apps.LandingConfig',
+    'jobs',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
