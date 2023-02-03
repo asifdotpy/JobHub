1077e22045e8d127f242547bd36b03704d5e6b97
asifdotpy <asifdotpy@gmail.com>
2023-02-03 18:28:46 +0600

    Version 1: index.html file deployed with django with all css property. It can be deployed now

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -117,6 +118,14 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
 STATIC_URL = 'static/'
+
+# Setting up static_root
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+
+# Setting up staticfiles directories
+STATICFILES_DIRS = [
+    BASE_DIR / "landing/templates/landing",
+        ]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
---
+++ mysite/static/README.md
@@ -0,0 +1,4 @@
+# Dotpot iT
+
+## Welcome to Dotpot iT, a full-service digital agency specializing in custom web and mobile app development. We are a team of skilled software developers, graphic designers, and digital marketing experts who are passionate about bringing your ideas to life.
+
