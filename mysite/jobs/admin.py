32fb81273ed3ac467ce2fa66c36816c3f9c74c52
asifdotpy <asifdotpy@gmail.com>
2023-03-09 17:19:23 +0600

    Registered Models into admin

--- mysite/jobs/admin.py
+++ mysite/jobs/admin.py
@@ -1,6 +1,10 @@
 # Register your models here.
 from django.contrib import admin
-from .models import FullStackDeveloper, DigitalMarketingManager
+from .models import FullStackDeveloper, DigitalMarketingManager, SeniorBackendEngineer, AIMLEngineer, GameDeveloper, MobileAppDeveloper
 
 admin.site.register(FullStackDeveloper)
 admin.site.register(DigitalMarketingManager)
+admin.site.register(SeniorBackendEngineer)
+admin.site.register(AIMLEngineer)
+admin.site.register(GameDeveloper)
+admin.site.register(MobileAppDeveloper)
